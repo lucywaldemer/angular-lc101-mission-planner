74bb2ae32d1bf67ffd243da903597535620fc2c0
Lu Waldemer <lucywaldemer@gmail.com>
2023-03-16 16:36:00 -0500

    finished studio

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -9,6 +9,7 @@ export class CrewComponent implements OnInit {
 
   inCrew: boolean = false;
   crew: object[] = [];
+  hoveredCandidate: object
 
   candidates: object[] = [
     {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
@@ -25,5 +26,34 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
+addCrewMember(candidate: object){
+
+  for (let i = 0; i < this.crew.length; i++){
+    if (this.crew[i]['name'] === candidate['name']){
+      // candidate exixts in the crew array
+      this.crew.splice(i,1);
+      return;
+    }
+  }
+  // if crew is less than 3
+    if (this.crew.length >= 3){
+      return;
+    }
+    // data added to crew array
+    this.crew.push(candidate);
+}
+
+isCandidateSelected(candidate: object): boolean {
+  for (let i = 0; i < this.crew.length; i++){
+    if (this.crew[i]['name'] === candidate['name']){
+      return true;
+    }
+    }
+  return false;
+  } 
+
+setMemberHovered(candidate: object){
+this.hoveredCandidate = candidate;
+}
 
 }
